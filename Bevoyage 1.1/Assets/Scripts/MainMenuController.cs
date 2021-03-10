fc8e56fdb97e1e9baf446820cb93f208b0cfc386
Christy Nguyen <Christy@Christys-MacBook-Pro.local>
2021-03-10 12:02:28 -0600

    Main Menu
    Added a scene to represent the Main Menu. Temporary placeholder background. Created UI for game prompts and options in preparations. Added scripts to buttons to toggle menus.

---
+++ Bevoyage 1.1/Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+	public GameObject mainMenu;
+    public GameObject optionsMenu;
+    public GameObject gameMenu;
+    
+    public void playGame() {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void gameSaves() {
+        mainMenu.SetActive(false);
+        gameMenu.SetActive(true);
+    }
+ 
+    public void options() {
+        mainMenu.SetActive(false);
+        optionsMenu.SetActive(true);
+    }
+
+    public void gameBack() {
+        mainMenu.SetActive(true);
+        gameMenu.SetActive(false);
+    }
+
+    public void optionsBack() {
+        mainMenu.SetActive(true);
+        optionsMenu.SetActive(false);
+    }
+ 
+    public void exitGame() {
+        Application.Quit();
+    }
+}
