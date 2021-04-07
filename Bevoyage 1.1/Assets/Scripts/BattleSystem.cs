50e87582a60379d8c54a9a4890f3dc12bc4d33e5
Christy Nguyen <christynguyen1299@gmail.com>
2021-04-07 10:56:45 -0500

    Battle Mechanics Update
    Added some more functions, awaiting placeholders

--- Bevoyage 1.1/Assets/Scripts/BattleSystem.cs
+++ Bevoyage 1.1/Assets/Scripts/BattleSystem.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public enum BattleState { START, PLAYERTURN, ENEMYTURN, WON, LOST }
 
+/* Future Notes: 
+ * May change enum states to be move select phase + damage phase.
+ * Refer to pseudocode sheet for game-specific mechanics.
+ * Scale up to include multiple characters.
+ */
+
 public class BattleSystem : MonoBehaviour
 {
 	public GameObject playerPrefab;
@@ -20,14 +26,45 @@ public class BattleSystem : MonoBehaviour
     void Start()
     {
         state = BattleState.START;
-        SetUpBattle();
+        StartCoroutine(SetUpBattle());
     }
 
-    void SetUpBattle() {
+    IEnumerator SetUpBattle() {
+        //Spawn players and enemies
     	GameObject playerGO = Instantiate(playerPrefab, playerStation);
     	playerUnit = playerGO.GetComponent<Unit>();
     	GameObject enemyGO = Instantiate(enemyPrefab, enemyStation);
     	enemyUnit = enemyGO.GetComponent<Unit>();
+
+        //Tutorial inserts code to change dialogue box to include enemy names, but may not need it
+
+        yield return new WaitForSeconds(3f);
+
+        //Change to Player Phase
+        state = BattleState.PLAYERTURN;
+        PlayerTurn();
     }
 
+    IEnumerator PlayerAttack() {
+        //deal damage
+
+        yield return new WaitForSeconds(3f);
+
+        //check if enemy is dead
+        //change state accordingly
+
+    }
+
+    //Method to represent player move selection.
+    void PlayerTurn() {
+
+    }
+
+    public void OnAttackButton() {
+        if (state != BattleState.PLAYERTURN) {
+            return;
+        } else {
+            StartCoroutine(PlayerAttack());
+        }
+    }
 }
