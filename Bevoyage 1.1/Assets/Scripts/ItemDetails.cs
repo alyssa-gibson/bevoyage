3cf88ad092a8bc68125ccf8f815f402abdb002a4
EmilyNB129 <50975820+EmilyNB129@users.noreply.github.com>
2021-04-08 22:33:27 -0500

    Item Prefab
    I created an item prefab so if we are able to implement items in the future we have the basic build for them.

---
+++ Bevoyage 1.1/Assets/Scripts/ItemDetails.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDetails : MonoBehaviour
+{
+    public GameObject item;
+    public string itemName;
+    public string itemDescription;
+}
